ea9742135a1704f3f1e6c4e54eb06d7aaa3716ce
henryhaller99 <henryhernandezbarajas@outlook.com>
2021-05-08 13:31:18 -0500

    end of section 3

---
+++ API/Controllers/CustomersController.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class CustomersController : BaseController
+    {
+        private readonly northwindContext _context;
+        public CustomersController(northwindContext context)
+        {
+            this._context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
+        {
+            return await _context.Customers.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Customer>> GetCustomer(string id)
+        {
+            return await _context.Customers.Where(x => x.CustomerId == id).FirstAsync(); 
+        }
+    }
+}
